e31c3a7099d590d69a10159f7ff3b1282d3b2508
Munene254 <midwaymaster10@gmail.com>
2023-02-27 11:09:06 +0300

    On closing it with forms.

---
+++ INTERMEDIATE HTML&CSS/form-validation/explore.js
@@ -0,0 +1,21 @@
+function validateForm(e) {
+    const form = e.target;
+    if (form.checkValidity()) {
+      // form is valid - make further checks
+    }
+    else {
+      // form is invalid - cancel submit
+      e.preventDefault();
+      // apply invalid class
+      Array.from(form.elements).forEach(i => {
+        if (i.checkValidity()) {
+          // field is valid - remove class
+          i.parentElement.classList.remove('invalid');
+        }
+        else {
+          // field is invalid - add class
+          i.parentElement.classList.add('invalid');
+        }
+      });
+    }
+  };
