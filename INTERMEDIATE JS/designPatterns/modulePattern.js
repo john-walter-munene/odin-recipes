a2da41ab4dcacbe4b3e57a9c6e22c6337a0a1c22
Munene254 <midwaymaster10@gmail.com>
2023-04-08 17:36:35 +0300

    On learning somehting new about patterns.

---
+++ INTERMEDIATE JS/designPatterns/modulePattern.js
@@ -0,0 +1,18 @@
+// Modules.
+
+// Math.js ES2015 Modules.
+const privateValue = 'This is a value private to the module.'
+
+export default function add(x, y) {
+    return x + y;
+  }
+function multiply(x) {
+    return x * 2;
+  }
+function subtract(x, y) {
+    return x - y;
+  }
+export function square(x) {
+    return x * x;
+  }
+
